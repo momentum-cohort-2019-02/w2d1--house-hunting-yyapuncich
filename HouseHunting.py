86f6b5f3f358d4893e1e8264fedab42d57387d49
Yifaa Yapuncich <yifaasy@gmail.com>
2019-02-18 16:37:28 -0500

    set variables

---
+++ HouseHunting.py
@@ -0,0 +1,9 @@
+annual_salary = int(input("Enter your annual salary: "))
+portion_saved = input("Enter percent of your salary to save, as a decimal: ")
+total_cost = int(input("Enter cost of your dream home: "))
+portion_down_payment = 0.25
+current_savings = 0
+r = 0.04
+monthly_investment = ((current_savings * r) / 12)
+
+while 
