45751a4e8d99720d804309911d2b05d34f7b7940
Yifaa Yapuncich <yifaasy@gmail.com>
2019-02-18 22:06:06 -0500

    omg it works now

--- HouseHunting.py
+++ HouseHunting.py
@@ -1,9 +1,20 @@
 annual_salary = int(input("Enter your annual salary: "))
-portion_saved = input("Enter percent of your salary to save, as a decimal: ")
+portion_saved = float(
+    input("Enter percent of your salary to save, as a decimal: "))
 total_cost = int(input("Enter cost of your dream home: "))
-portion_down_payment = 0.25
+portion_down_payment = float(0.25 * total_cost)
 current_savings = 0
 r = 0.04
-monthly_investment = ((current_savings * r) / 12)
+interest = float(current_savings * r)
+months = 0
 
-while 
+# calculate how many months it will take to save down payment
+
+while current_savings < portion_down_payment:
+    current_savings += interest + (float(portion_saved) * (annual_salary / 12))
+    interest = float(current_savings * r)
+    months += 1
+    # print(current_savings)
+
+    if current_savings >= portion_down_payment:
+        print("Number of months to reach your dream: " + str(months))
